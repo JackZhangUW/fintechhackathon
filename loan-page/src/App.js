03f430a1b2afeff007599b8f462bb32304c4362a
zhouyifan0904 <zhouyf98@uw.edu>
2019-04-13 19:18:10 -0700

    save

--- loan-page/src/App.js
+++ loan-page/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 // bootstrap
 import 'bootstrap/dist/css/bootstrap.css';
@@ -22,7 +21,15 @@ export default class App extends Component {
 		}
 	}
 
-	signIn = (email, password) => {
+	completeSignIn = () => {
+		this.setState({
+			signIn: false,
+			loanForm: true
+		});
+		console.log('success');
+	}
+
+	signUp = (email, password) => {
 		firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
 			// Handle Errors here.
 			var errorCode = error.code;
@@ -30,12 +37,16 @@ export default class App extends Component {
 			// ...
 			console.log(errorMessage);
 			console.log('email: ' + email);
-		}).then(() => {
-			this.setState({
-				signIn: false,
-				loanForm: true
-			})
-		})
+		}).then(() => this.completeSignIn);
+	}
+
+	signIn = (email, password) => {
+		firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+			// Handle Errors here.
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			// ...
+		}).then(() => this.completeSignIn);
 	}
 
 	render() {
@@ -43,7 +54,7 @@ export default class App extends Component {
 		let builder = <div></div>;
 
 		if (this.state.signIn) {
-			builder = <SignInForm signIn={this.signIn} />;
+			builder = <SignInForm signIn={this.signIn} signUp={this.signUp} />;
 		} else if (this.state.loanForm) {
 			builder = <LoanForm />;
 		} else if (this.state.loanOffer) {
@@ -52,7 +63,7 @@ export default class App extends Component {
 
 		return (
 			<div className="App">
-				<SignInForm signIn={this.signIn} />
+				{builder}
 			</div>
 		);
 	}
@@ -60,6 +71,7 @@ export default class App extends Component {
 
 /*
 props:
+	signUp(email, password)
 	signIn(email, password)
 
 */
@@ -84,6 +96,10 @@ class SignInForm extends Component {
 		});
 	}
 
+	signUp = () => {
+		this.props.signUp(this.state.email, this.state.password);
+	}
+
 	signIn = () => {
 		this.props.signIn(this.state.email, this.state.password);
 	}
@@ -104,8 +120,11 @@ class SignInForm extends Component {
 					<Form.Label>Password</Form.Label>
 					<Form.Control type="password" placeholder="Password" onChange={this.updatePassword} />
 				</Form.Group>
+				<Button variant="primary" type="submit" onClick={this.signUp}>
+					Sign Up
+				</Button>
 				<Button variant="primary" type="submit" onClick={this.signIn}>
-					Submit
+					Sign In
 				</Button>
 			</Form>
 		);
@@ -122,20 +141,62 @@ class LoanForm extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			email: ''
-
+			result: ''
 		}
+	}
+
+	apiCall = () => {
+		const ACCESS_CODE = "e7675dd3-ff3b-434b-95aa-70251cc3784b_88140dd4-f13e-4ce3-8322-6eaf2ee9a2d2";
+		const POST_URL = "https://api.evenfinancial.com/leads/rateTables";
+
+		let data = JSON.stringify({
+			"productTypes": [
+					"loan",
+					"savings"
+			],
+			"personalInformation": {
+					"firstName": "John",
+					"lastName": "Doe",
+					"email": "john@example.com",
+					"city": "New York",
+					"state": "NY",
+					"workPhone": "2125551234",
+					"primaryPhone": "2125556789",
+					"address1": "45 West 21st Street",
+					"address2": "5th Floor",
+					"zipcode": "10010",
+					"monthsAtAddress": 5,
+					"driversLicenseNumber": "111222333",
+					"driversLicenseState": "NY",
+					"ipAddress": "8.8.8.8",
+					"activeMilitary": false,
+					"militaryVeteran": true,
+					"dateOfBirth": "1993-10-09",
+					"educationLevel": "bachelors",
+					"ssn": "111-22-3333"
+			}
+		});
+
+		let myHeader = new Headers();
+		myHeader.append('Content-Type', 'application/json');
+		myHeader.append('Authorization', 'Bearer e7675dd3-ff3b-434b-95aa-70251cc3784b_88140dd4-f13e-4ce3-8322-6eaf2ee9a2d2');
+		myHeader.append('mode', 'cors');
+		
+		fetch("https://api.evenfinancial.com/leads/rateTables", {
+				method: 'POST',
+				headers: myHeader,
+				body: data
+		})
+		.then(res => res.json())
+		.then(response => {this.setState({result: response})})
+		.catch(error => console.error('Error:', error));
 	}
 
 	render() {
 		return (
-			<form>
-				<div class="form-group">
-					<label for="exampleInputEmail1">Email address</label>
-					<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-					<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-				</div>
-			</form>
+			<div>
+				{this.state.result}
+			</div>
 		)
 	}
 }
