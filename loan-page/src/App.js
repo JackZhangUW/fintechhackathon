7741a8f79231b1991418181d6ece45ec2ffef496
Sam Lee <minhsuan@cs.washington.edu>
2019-04-14 05:16:51 -0700

    trash

--- loan-page/src/App.js
+++ loan-page/src/App.js
@@ -140,7 +140,7 @@ export default class App extends Component {
 		if (this.state.signIn) {
 			builder = <SignInForm signIn={this.signIn} signUp={this.signUp} />;
 		} else if (this.state.loanForm) {
-			builder = <PersonalInformationForm />
+			builder = <PersonalInformationForm />;
 			//console.log(builder);
 		} else if (this.state.loanOffer) {
 			builder = <LoanOffer getResult={this.getResult} result={this.state.result} />;
@@ -410,10 +410,11 @@ class PersonalInformationForm extends Component {
 		const ACCESS_CODE = "e7675dd3-ff3b-434b-95aa-70251cc3784b_88140dd4-f13e-4ce3-8322-6eaf2ee9a2d2";
 		const POST_URL = "https://api.evenfinancial.com/leads/rateTables";
 
-		let data = JSON.stringify(this.makeRequestBody());
+		let data = JSON.stringify(makeRequestBody());
 		let myHeader = new Headers();
 		myHeader.append('Content-Type', 'application/json');
 		myHeader.append('Authorization', 'Bearer e7675dd3-ff3b-434b-95aa-70251cc3784b_88140dd4-f13e-4ce3-8322-6eaf2ee9a2d2');
+		// myHeader.append('mode', 'cors');
 
 		fetch("https://api.evenfinancial.com/leads/rateTables", {
 				method: 'POST',
